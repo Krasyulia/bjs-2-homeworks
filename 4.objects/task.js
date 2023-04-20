187f7170282c61d7f0e7b98ad9c4f77c5c5c5f37
Krasyulia <krasjulia@gmail.com>
2023-04-21 00:07:22 +0400

    incapsulation of student

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(Array.isArray(this.marks)) {
+    this.marks.push(mark);
+  } else {
+    this.marks = [mark];
+  }
+}
+
+Student.prototype.addMarks = function () {
+  if(Array.isArray(this.marks)) {
+    this.marks = this.marks.concat([...arguments]);
+  } else {
+    this.marks = [...arguments];
+  }
+}
+
+Student.prototype.getAverage = function () {
+  if(Array.isArray(this.marks)) {
+    return this.marks.reduce((prev, curr) => prev + curr, 0) / this.marks.length;
+  } else {
+    return 0;
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
+
+  
